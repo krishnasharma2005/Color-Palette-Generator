71213f1a4a32699cc1e246c1e1a8dc8d06f21198
Krishna Sharma <krishnasharma3011@gmail.com>
2025-04-27 22:27:44 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ColorPaletteGenerator from './ColorPaletteGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorPaletteGenerator />
+    </div>
+  );
+}
+
+export default App;
